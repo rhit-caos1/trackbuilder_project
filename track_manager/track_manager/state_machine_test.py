8c66f34ed1b43b7fd2091668a7984d9612d20227
slothyshane <shanedeng@cmu.edu>
2023-12-05 14:55:06 -0600

    modified state_machine_test

--- track_manager/track_manager/state_machine_test.py
+++ track_manager/track_manager/state_machine_test.py
@@ -11,23 +11,33 @@ class StateMachine():
     state = ['waiting', 'home', 'scan', 'printing', 'grasp', 'place', 'track']
 
     def __init__(self):
-        super().__init__('state_machine')
-        self.machine = Machine(model = self, states = StateMachine.state, initial = 'waiting')
+        # main condition checks
+        self.arm_process_complete = True # this changes to false whenever we send a service request to the arm
+        self.printer_process_complete = True # this changes to false whenever we send a service request to the printer
+
+        # sub condition checks
+        self.scan_all_tags = False
+
+        self.machine = Machine(model = self, states = StateMachine.state, initial = 'waiting', queued=True)
 
         # define state transitions
         # define the trigger as mach_trig_NAME
-        self.machine.add_transition(trigger='mach_trig_homing', source='*', dest='home')
-        self.machine.add_transition(trigger='mach_trig_waiting', source='*', dest='waiting')
-
+        self.machine.add_transition(trigger='mach_trig_homing', source='*', dest='home', conditions=['is_arm_process_complete'])
+        self.machine.add_transition(trigger='mach_trig_waiting', source='*', dest='waiting', conditions=['is_arm_process_complete'])
+        
         self.machine.add_transition(trigger='mach_trig_scan', source='wait', dest='scan')
+        self.machine.add_transition(trigger='mach_trig_trackGen', source='scan', dest='track', conditions=['is_scan_all_tags'])
 
         
         # define state functions
         # define the state as mach_state_NAME
         self.machine.on_enter_home('mach_state_home')
+        self.machine.on_enter_waiting('mach_state_waiting')
+        self.machine.on_enter_scan('mach_state_scan')
+        self.machine.on_enter
 
         # define service       
-    
+
         # define publisher
 
         # define subscriber
@@ -35,18 +45,29 @@ class StateMachine():
         # define action
 
         # define timer 
-        
-        self.get_logger().info("State machine initialized!")
-        self.get_logger().info("current state " + self.state)
+
+    def is_arm_process_complete(self):
+        return self.arm_process_complete
     
-    def homing_callback(self, request, response):
-        if request.data == True:
-            response.success = True
-            self.mach_trig_homing()
-        return response
+    def is_printer_process_complete(self):
+        return self.printer_process_complete
     
+    def is_scan_all_tags(self):
+        return self.scan_all_tags
+
+
     def mach_state_home(self):
         # call the service to go home
+        print("going home")
+    
+    def mach_state_waiting(self):
+        print("waiting")
+    
+    def mach_state_scan(self):
+        self.arm_process_complete = False
+        # call the service to scan
+        print("scanning")
 
-        print("")
 
+state = StateMachine()
+state.mach_trig_homing()
