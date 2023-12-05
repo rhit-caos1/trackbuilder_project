49bd326461d0a95d4f0af83e29c4111b61216a77
slothyshane <shanedeng@cmu.edu>
2023-12-05 14:07:09 -0600

    modified state machine

--- track_manager/track_manager/state_machine.py
+++ track_manager/track_manager/state_machine.py
@@ -13,20 +13,19 @@ import os
 class StateMachine(Node):
 
     # define all the states used in the state machine
-    state = ['home', 'scan_coarse', 'scan_fine', 'printing', 'grasp', 'place']
+    state = ['waiting', 'home', 'scan', 'printing', 'grasp', 'place', 'track']
 
     def __init__(self):
         super().__init__('state_machine')
-        self.machine = Machine(model = self, states = StateMachine.state, initial = 'scan')
+        self.machine = Machine(model = self, states = StateMachine.state, initial = 'waiting')
 
         # define state transitions
         # define the trigger as mach_trig_NAME
         self.machine.add_transition(trigger='mach_trig_homing', source='*', dest='home')
-        self.machine.add_transition(trigger='mach_trig_scan_coarse', source='home', dest='scan_coarse')
-        self.machine.add_transition(trigger='mach_trig_scan_fine', source='scan_coarse', dest='scan_fine')
-        self.machine.add_transition(trigger='mach_trig_printing', source='scan_fine', dest='printing')
-        self.machine.add_transition(trigger='mach_trig_grasp', source='printing', dest='grasp')
-        self.machine.add_transition(trigger='mach_trig_place', source='grasp', dest='place')
+        self.machine.add_transition(trigger='mach_trig_waiting', source='*', dest='waiting')
+
+        self.machine.add_transition(trigger='mach_trig_scan', source='wait', dest='scan')
+
         
         # define state functions
         # define the state as mach_state_NAME
