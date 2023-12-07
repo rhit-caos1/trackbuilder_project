f29c8a7df2d9e841ac827d40643876f9ac07e8e4
slothyshane <shanedeng@cmu.edu>
2023-12-07 16:25:25 -0600

    updated state machine

--- track_manager/track_manager/state_machine.py
+++ track_manager/track_manager/state_machine.py
@@ -7,33 +7,36 @@ from std_msgs.msg import String
 from std_msgs.msg import Bool
 import numpy as np
 from std_srvs.srv import SetBool
-from transitions import Machine
 import os
+from enum import Enum, auto
+from movebot_interfaces.srv import TrackPoints
+
+# define all the possible states
+class State(Enum):
+    WAITING = auto()
+    HOME = auto()
+    SCAN = auto()
+    PRINTING = auto()
+    GRASP = auto()
+    TRACK = auto()
 
 class StateMachine(Node):
-
-    # define all the states used in the state machine
-    state = ['waiting', 'home', 'scan', 'printing', 'grasp', 'place', 'track']
-
     def __init__(self):
         super().__init__('state_machine')
-        self.machine = Machine(model = self, states = StateMachine.state, initial = 'waiting')
-
+        # initialize the machine
+        self.state = State.WAITING
+        self.all_region_scanned = False
+        self.tag_location = []
+        self.scan_region = np.array([[0.0, 0.0, False], [0.0, 0.0, False]]) # define the scan region
         # define state transitions
-        # define the trigger as mach_trig_NAME
-        self.machine.add_transition(trigger='mach_trig_homing', source='*', dest='home')
-        self.machine.add_transition(trigger='mach_trig_waiting', source='*', dest='waiting')
-
-        self.machine.add_transition(trigger='mach_trig_scan', source='wait', dest='scan')
-
-        
+       
         # define state functions
-        # define the state as mach_state_NAME
-        self.machine.on_enter_home('mach_state_home')
 
         # define service
-        self.homing_service = self.create_service(SetBool, 'go_home', self.homing_callback)
-       
+        self.home_service = self.create_client(SetBool, 'home_service')
+        self.start_robot_service = self.create_server(SetBool, 'start_robot_service', self.start_robot_callback)
+        self.scan_service = self.create_client(TrackPoints, 'scan_service') # change the service type
+        self.track_service = self.create_client(SetBool, 'track_service') # change the service type
     
         # define publisher
 
@@ -42,22 +45,116 @@ class StateMachine(Node):
         # define action
 
         # define timer 
-        
+        self.timer = self.create_timer(1, self.timer_callback) # update the state every 1 second
+
         self.get_logger().info("State machine initialized!")
         self.get_logger().info("current state " + self.state)
     
-    def homing_callback(self, request, response):
-        if request.data == True:
+    ########Service Callbacks########
+    def timer_callback(self):
+        self.get_logger().info("current state " + self.state)
+        if self.state == State.WAITING:
+            self.waiting()
+        elif self.state == State.HOME:
+            self.home()
+        elif self.state == State.SCAN:
+            self.scan()
+        elif self.state == State.TRACK:
+            self.track()
+        elif self.state == State.PRINTING:
+            self.printing()
+        elif self.state == State.GRASP:
+            self.grasp()
+
+    def start_robot_callback(self, request, response):
+        if request.data:
+            self.state = State.HOME
             response.success = True
-            self.mach_trig_homing()
+            response.message = "Robot started"
+        else:
+            response.success = False
+            response.message = "Robot failed to start"
         return response
     
-    def mach_state_home(self):
-        # call the service to go home
+    ########Condition checks########
 
-        self.get_logger().info("Going home")
+    
+    ########State Functions########
+    def waiting(self):
+        # do nothing
+        pass
+
+    def home(self):
+        # move to home position
+        rqst = SetBool.Request()
+        rqst.data = True
+        response = self.home_service.call(rqst)
+        if response.success:
+            self.get_logger().info("Home service success")
+            self.state = State.SCAN
+        else:
+            self.get_logger().info("Home service failed")
+            self.state = State.WAITING
+
+    def scan(self):
+        # loop through the scan region
+        point_scan_next = None
+        for point in self.scan_region:
+            if point[2]:
+               continue
+            else:
+                point_scan_next = point
+                self.scan_region[point] = True #
+        
+        if point_scan_next is None:
+            self.all_region_scanned = True
+            self.state = State.TRACK
+            return 
+        
+        else:
+            # call the scan service
+            rqst = TrackPoints.Request()
+            rqst.x = point_scan_next[0]
+            rqst.y = point_scan_next[1]
+            response = self.scan_service.call(rqst)
+            # save this response to a file
+            
+            # TO DO: do a LMSR to update the points 
+            self.tag_location.append([response.x, response.y, response.theta])
+            open("points.txt", "a").write(response)
+            if response.success:
+                self.get_logger().info("Scan service success")
+            else:
+                self.get_logger().info("Scan service failed")
+                self.state = State.WAITING
 
 
+    def track(self):
+        rqst = SetBool.Request()   
+        rqst.data = True
+        response = self.track_service.call(rqst)
+        if response.success:
+            self.get_logger().info("Track service success")
+            self.state = State.PRINTING
+        else:
+            self.get_logger().info("Track service failed")
+            self.state = State.WAITING
+
+
+    def printing(self):
+        pass
+
+    def grasp(self):
+        pass
+
+        # self.future = self.home_service.call_async(rqst)
+        # rclpy.spin_until_future_complete(self, self.home_service)
+        # if self.future.result() is not None:
+        #     self.get_logger().info("Home service success")
+        #     self.state = State.WAITING
+        # else:
+        #     self.get_logger().info("Home service failed")
+        # pass
 
 def main(args=None):
     rclpy.init(args=args)
--- track_manager/track_manager/state_machine_test.py
+++ track_manager/track_manager/state_machine_test.py
@@ -8,7 +8,7 @@ import os
 class StateMachine():
 
     # define all the states used in the state machine
-    state = ['waiting', 'home', 'scan', 'printing', 'grasp', 'place', 'track']
+    state = ['waiting', 'home', 'scan', 'printing', 'grasp', 'track']
 
     def __init__(self):
         # main condition checks
@@ -24,9 +24,13 @@ class StateMachine():
         # define the trigger as mach_trig_NAME
         self.machine.add_transition(trigger='mach_trig_homing', source='*', dest='home', conditions=['is_arm_process_complete'])
         self.machine.add_transition(trigger='mach_trig_waiting', source='*', dest='waiting', conditions=['is_arm_process_complete'])
+        self.machine.add_transition(trigger='', source='home', dest='waiting', conditions=['is_arm_process_complete'])
         
         self.machine.add_transition(trigger='mach_trig_scan', source='wait', dest='scan')
         self.machine.add_transition(trigger='mach_trig_trackGen', source='scan', dest='track', conditions=['is_scan_all_tags'])
+        self.machine.add_transition(trigger='mach_trig_print', source='track', dest='printing', conditions=['is_printer_process_complete'])
+        self.machine.add_transition(trigger='mach_trig_grasp', source='printing', dest='grasp', conditions=['is_arm_process_complete', 'is_printer_process_complete'])
+        self.machine.add_transition(trigger='mach_trig_place', source='grasp', dest='scan', conditions=['is_arm_process_complete'])
 
         
         # define state functions
@@ -34,13 +38,17 @@ class StateMachine():
         self.machine.on_enter_home('mach_state_home')
         self.machine.on_enter_waiting('mach_state_waiting')
         self.machine.on_enter_scan('mach_state_scan')
-        self.machine.on_enter
+        self.machine.on_enter_track('mach_state_track')
+        self.machine.on_enter_printing('mach_state_printing')
+        self.machine.on_enter_grasp('mach_state_grasp')
 
         # define service       
+        
 
         # define publisher
 
         # define subscriber
+        # we need a printer status subscriber and update the printer_process_complete variable
 
         # define action
 
@@ -54,11 +62,13 @@ class StateMachine():
     
     def is_scan_all_tags(self):
         return self.scan_all_tags
-
+    
 
     def mach_state_home(self):
         # call the service to go home
+        self.arm_process_complete = False
         print("going home")
+        self.arm_process_complete = True
     
     def mach_state_waiting(self):
         print("waiting")
@@ -67,6 +77,20 @@ class StateMachine():
         self.arm_process_complete = False
         # call the service to scan
         print("scanning")
+
+    def mach_state_track(self):
+        # call the service to generate the track
+        print("generating track")
+
+    def mach_state_printing(self):
+        self.printer_process_complete = False
+        # call the service to print
+        print("printing")
+    
+    def mach_state_grasp(self):
+        self.arm_process_complete = False
+        # call the service to grasp
+        print("grasping")
 
 
 state = StateMachine()
