f4c611ca8751bbccfe8eb018e0c0af241d09c1fc
shanedeng <shanedeng@cmu.edu>
2023-12-24 15:06:26 -0600

    modified state machine. Tested the scan step

--- track_manager/track_manager/sm_test.py
+++ track_manager/track_manager/sm_test.py
@@ -6,39 +6,52 @@ from math import pi
 from std_msgs.msg import String
 from std_msgs.msg import Bool
 import numpy as np
-from std_srvs.srv import SetBool
+from std_srvs.srv import SetBool, Empty
 import os
 from enum import Enum, auto
 from movebot_interfaces.srv import TrackPoints, ScanPoints
+import random
 
 
 
 class Test(Node):
     def __init__(self):
         super().__init__('Test')
-
+        self.scan_counter = 0
         self.home_service = self.create_service(SetBool, 'home_service', self.home_callback)
         self.scan_service = self.create_service(ScanPoints, 'scan_service', self.scan_callback)
         self.track_service = self.create_service(TrackPoints, 'track_service', self.track_callback)
         self.print_service = self.create_service(SetBool, 'print_service', self.print_callback)
+        self.grasp_service = self.create_service(ScanPoints, 'grasp_service', self.grasp_callback)
+        self.place_service = self.create_service(ScanPoints, 'place_service', self.place_callback)
 
     def home_callback(self, request, response):
         self.get_logger().info("home callback")
-        response.success = True
-        response.message = "home success"
         self.get_logger().info("complete")
+        response.success = True
         return response
     
     def scan_callback(self, request, response):
         self.get_logger().info("scan callback")
-        response.x = 0 
-        response.y = 0
-        response.theta = 0
+
+        if self.scan_counter == 0:
+            response.x = -5.0
+            response.y = 5.0  # random int between 0 and 10
+            response.theta = 0.0
+            response.success = True
+        elif self.scan_counter == 1:
+            response.x = 5.0
+            response.y = 5.0
+            response.theta = 0.0
+            response.success = True
+
+        self.scan_counter += 1
         return response
     
     def track_callback(self, request, response):
         self.get_logger().info("track callback")
-        response.success = True
+        response.last_piece = False
+
         return response
     
     def print_callback(self, request, response):
@@ -46,6 +59,23 @@ class Test(Node):
         response.success = True
         return response
     
+    def grasp_callback(self, request, response):
+        self.get_logger().info("grasp callback")
+        response.x = 0.0
+        response.y = 0.0
+        response.theta = 0.0
+        response.success = True
+        return response
+    
+    def place_callback(self, request, response):
+        self.get_logger().info("place callback")
+        response.x = 0.0
+        response.y = 0.0
+        response.theta = 0.0
+        response.success = True
+        return response
+    
+
 def main(args=None):
     rclpy.init(args=args)
     node = Test()
--- track_manager/track_manager/state_machine.py
+++ track_manager/track_manager/state_machine.py
@@ -20,10 +20,18 @@ class State(Enum):
     PRINTING = auto()
     GRASP = auto()
     TRACK = auto()
+    PLACE = auto()
 
 class StateMachine(Node):
     def __init__(self):
         super().__init__('state_machine')
+
+        ########PARAMETERS########
+        self.scan_region = np.array([[0.0, 0.0, False], [1.0, 0.0, False]]) # define the scan region
+        self.tolerance = 0.2 # define the tolerance for the scan region, the unit is in m
+        self.printer_scan_location = [0.0,0.0]
+        ##########################
+
         # initialize the machine
         self.state = State.WAITING
         self.in_motion = False
@@ -33,29 +41,49 @@ class StateMachine(Node):
         self.cbgroup_timer = MutuallyExclusiveCallbackGroup()
         self.all_region_scanned = False
         self.tag_location = [] # list of tag locations
+        self.track_end = [] # list of track end locations
+        self.left_pointer = 0 # left pointer for the tag location
+        self.right_pointer = 1 # right pointer for the tag location
+        self.initial_scan = True
+        self.track_end_generated_last = []
+        self.end_left = []
+        self.start_right = []
+
         # open points.txt and read the points
         self.current_dir = os.getcwd()
         with open(self.current_dir + "/src/track_manager/track_manager/points.txt", "r") as f:
             for line in f:
-                self.tag_location.append(line.split(" "))
+                # split the line by space
+                line = line.split(" ")
 
-        self.scan_region = np.array([[0.0, 0.0, False], [0.0, 0.0, False]]) # define the scan region
-        self.tolerance = 0.3 # define the tolerance for the scan region, the unit is in m
-        
-        # define state transitions
-       
-        # define state functions
+                # convert it to float for the tag location
+                # note that this is in the camera frame 
+                tag = [float(line[0]), float(line[1]), float(line[2])]
+                self.tag_location.append(tag)
+
+                
+        with open(self.current_dir + "/src/track_manager/track_manager/track_end.txt", "r") as f:
+            for line in f:
+                # split the line by space
+                # note that this in the track frame
+                line = line.split(" ")
+                track_end = [float(line[0]), float(line[1]), float(line[2])]
+                self.track_end.append(track_end)   
 
         # define service
         self.home_service = self.create_client(SetBool, 'home_service')
         self.start_robot_service = self.create_service(Empty, 'start_robot_service', self.start_robot_callback)
-        self.scan_service = self.create_client(ScanPoints, 'scan_service') # change the service type
-        self.track_service = self.create_client(TrackPoints, 'track_service') # change the service type
+        self.scan_service = self.create_client(ScanPoints, 'scan_service')
+        self.track_service = self.create_client(TrackPoints, 'track_service') 
         self.print_service = self.create_client(SetBool, 'print_service') # change the service type
+        self.grasp_service = self.create_client(ScanPoints, 'grasp_service') # change the service type
+        self.place_service = self.create_client(ScanPoints, 'place_service') # change the service type
         
+        self.get_logger().info('starting services')
         self.home_service.wait_for_service()
         self.scan_service.wait_for_service()
         self.track_service.wait_for_service()
+        self.grasp_service.wait_for_service()
         self.get_logger().info('all services are ready')
         # define publisher
 
@@ -86,17 +114,17 @@ class StateMachine(Node):
         elif self.state == State.TRACK:
             await self.track()
         elif self.state == State.PRINTING:
-            await self.printing()
+            self.printing()
         elif self.state == State.GRASP:
             await self.grasp()
+        elif self.state == State.PLACE:
+            await self.place()
 
     def start_robot_callback(self, request, response):
         self.state = State.HOME
-        self.get_logger().info("start robot ")
+        self.get_logger().info("start robot")
         return response
     
-    ########Condition checks########
-
     
     ########State Functions########
     def waiting(self):
@@ -132,10 +160,12 @@ class StateMachine(Node):
                 point_scan_next = self.scan_region[i]
                 # set the original point to true
                 self.scan_region[i][2] = True
+                break
         # transition to track if all the points have been scanned
         if point_scan_next is None:
             self.all_region_scanned = True
             self.state = State.TRACK
+            self.initial_scan = False
             return 
         
         else:
@@ -160,8 +190,28 @@ class StateMachine(Node):
                     self.tag_location[i][2] = theta
                     exist = True
                     break
+
             # check 
-            if not exist:
+            if not exist and self.initial_scan == False:
+                tag_pose = [x, y, theta]
+                self.tag_location.insert(self.left_pointer,tag_pose)
+                self.track_end.insert(self.left_pointer,self.track_end_generated_last)
+
+                 # write the points to the file
+                with open(self.current_dir + "/src/track_manager/track_manager/points.txt", "w") as f:
+                    f.seek(0)
+                    f.truncate()
+                    for pose in self.tag_location:
+                        f.write(str(pose[0]) + " " + str(pose[1]) + " " + str(pose[2]) + "\n")
+
+                # also change the track end
+                with open(self.current_dir + "/src/track_manager/track_manager/track_end.txt", "w") as f:
+                    f.seek(0)
+                    f.truncate()
+                    for track in self.track_end:
+                        f.write(str(track[0]) + " " + str(track[1]) + " " + str(track[2]) + "\n")
+            
+            elif not exist and self.initial_scan == True:
                 tag_pose = [x, y, theta]
                 self.tag_location.append(tag_pose)
                 
@@ -170,6 +220,8 @@ class StateMachine(Node):
                     for pose in self.tag_location:
                         f.write(str(pose[0]) + " " + str(pose[1]) + " " + str(pose[2]) + "\n")
 
+            else:
+                self.get_logger().info("Point already exists")
             if self.future.success:
                 self.get_logger().info("Scan service success")
             else:
@@ -178,14 +230,31 @@ class StateMachine(Node):
 
     async def track(self):
         rqst = TrackPoints.Request()  
-        # TO DO: need to keep track of what tags have already been generated and printed
+        # check if the length of the tag_location is greater than 2
+        if len(self.tag_location) < 2:
+            self.get_logger().info("Not enough tracks")
+            self.state = State.WAITING
+            return
+        
+        # transformation matrix:
+        self.end_left = self.global_end(self.tag_location[self.left_pointer])
+        self.start_right = self.global_start(self.tag_location[self.right_pointer])
+        
+        tag_pos = [self.end_left[0], self.end_left[1], self.start_right[0], self.start_right[1]]
+        tag_angle = [self.end_left[2], self.self.start_right[2]]
+
+        rqst.position = tag_pos
+        rqst.angle = tag_angle
+
         self.future = await self.track_service.call_async(rqst)
         self.print_name = self.future.file_names
+        self.track_end_generated_last = self.future.track_end   
 
         if self.future.last_piece == True:
-            # that means we have finished printing all the pieces for this tag
-            # TODO: manage the tag
-            pass
+            self.right_pointer += 1
+            self.left_pointer += 1
+        else:
+            self.left_pointer += 1
 
         if self.print_name is not None:
             self.get_logger().info("Track service success")
@@ -196,19 +265,60 @@ class StateMachine(Node):
 
     def printing(self):
         # call the print service
-        pass
 
-    def grasp(self):
+        # skip this state and go to grasp   
+        # self.state = State.GRASP
         pass
+        
 
-        # self.future = self.home_service.call_async(rqst)
-        # rclpy.spin_until_future_complete(self, self.home_service)
-        # if self.future.result() is not None:
-        #     self.get_logger().info("Home service success")
-        #     self.state = State.WAITING
-        # else:
-        #     self.get_logger().info("Home service failed")
-        # pass
+    async def grasp(self):
+        rqst = ScanPoints.Request()
+        rqst.x = self.printer_scan_location[0]
+        rqst.y = self.printer_scan_location[1]
+        self.future = await self.grasp_service.call_async(rqst)
+
+        if self.future.success:
+            self.get_logger().info("Grasp service success")
+            self.state = State.PLACE
+            
+    async def place(self):
+        # perform the transformation
+        # call the place service
+        rqst = ScanPoints.Request()
+
+        rqst.x = 0.0
+        rqst.y = 0.0
+        rqst.theta = 0.0
+        self.future = await self.place_service.call_async(rqst)
+
+        if self.future.success:
+            self.get_logger().info("Place service success")
+            self.scan_region.append([self.future.x, self.future.y, False])
+            self.state = State.SCAN
+    
+    ########Helper Functions########
+    def global_start(self, tag_location):
+        start_tag = [-0.5, 0.5, 1.0]
+        matrix_transformation_global2tag = np.array([[np.cos(tag_location[2]), -np.sin(tag_location[2]), tag_location[0]],
+                                                     [np.sin(tag_location[2]), np.cos(tag_location[2]), tag_location[1]],
+                                                     [0.0, 0.0, 1.0]]) 
+
+        start_global = np.matmul(np.inv(matrix_transformation_global2tag), start_tag)
+        # print out the location of the start
+        self.get_logger().info("start location in global frame: " + str(start_global))
+        start_global = [start_global[0], start_global[1], tag_location[2]] 
+        return start_global
+
+    def global_end(self, tag_location, end_location):
+        end_tag = end_location
+        matrix_transformation_global2tag = np.array([[np.cos(tag_location[2]), -np.sin(tag_location[2]), tag_location[0]],
+                                                     [np.sin(tag_location[2]), np.cos(tag_location[2]), tag_location[1]],
+                                                     [0.0, 0.0, 1.0]])
+        end_global = np.matmul(np.inv(matrix_transformation_global2tag), end_tag)
+        # print out the location of the end
+        self.get_logger().info("end location in global frame: " + str(end_global))
+        end_global = [end_global[0], end_global[1], tag_location[2] + end_location[2]]
+        return end_global
 
 def main(args=None):
     rclpy.init(args=args)
